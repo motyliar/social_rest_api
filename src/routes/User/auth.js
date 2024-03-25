38ab4ae0a37ec76900d596ab8a7a0ba6aa723ae0
root <root@b119.mikr.us>
2024-03-25 21:41:25 +0100

    webhook function update

--- src/routes/User/auth.js
+++ src/routes/User/auth.js
@@ -1,3 +1,4 @@
+const { exec } =require('child_process');
 const express = require('express');
 const authenticate = require('../../controllers/Authenticate/auth_controller');
 const router = express.Router();
@@ -6,9 +7,24 @@ router.post('/register', authenticate.userRegister);
 router.post('/login', authenticate.login);
 router.post('/logout', authenticate.logout);
 router.post('/hook', async (req, res) => {
-    console.log('This is webhook');
-    res.status(200).send("OK");
-})
+    console.log('wchodze w exec');
+    try {
+        await new Promise((resolve, reject) => {
+            exec('sh /root/.restart.sh', (error, stdout, stderr) => {
+                if(error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        });
+        console.log('This is webhook');
+        res.status(200).send("OK");
+    } catch (err) {
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 
 module.exports = router;
